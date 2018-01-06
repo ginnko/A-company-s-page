b0ca7c9b04e0071f981177a4c3d69dcb146f1e9e
ginnko <yybnbn@126.com>
2018-01-07 01:27:14 +0800

    fix code in gulpfile.js with browsersync getting page reload automatically

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,18 @@
 var gulp = require('gulp');
 var fileinclude = require('gulp-file-include');
-var livereload = require('gulp-livereload');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browser-sync', ['css', 'script', 'image', 'font', 'fileinclude'], function(){
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  gulp.watch('./src/css/*.css', ['css']);
+  gulp.watch('./src/js/*.js', ['script']);
+  gulp.watch('./src/*.html', ['fileinclude']);
+  gulp.watch("./dist").on('change', browserSync.reload);
+});
 
 gulp.task('fileinclude', function(){
   gulp.src('./src/*.html')
@@ -9,40 +21,33 @@ gulp.task('fileinclude', function(){
     basepath: '@file'
   }))
   .pipe(gulp.dest('dist'))
-  .pipe(livereload());
+  .pipe(browserSync.stream());
 });
 
 gulp.task('css', function(){
   gulp.src('./src/css/*.css')
   .pipe(gulp.dest('dist/css'))
-  .pipe(livereload());
+  .pipe(browserSync.stream());
 });
 
 gulp.task('script', function(){
   gulp.src('./src/js/*.js')
   .pipe(gulp.dest('dist/js'))
-  .pipe(livereload());
+  .pipe(browserSync.stream());
 });
 
 gulp.task('image', function(){
   gulp.src(['./src/img/product/*.*', './src/img/header/*.*', './src/img/footer/*.*', './src/img/index/*.*', './src/img/product/uos-enterprise/*.*', './src/img/product/uos-operator/*.*', './src/img/product/uos-director/*.*', './src/img/product/uos-manager/*.*'])
   .pipe(gulp.dest('dist/img'))
-  .pipe(livereload());
+  .pipe(browserSync.stream());
 });
 
 gulp.task('font', function(){
   gulp.src('./src/fonts/*.*')
   .pipe(gulp.dest('dist/fonts'))
-  .pipe(livereload());
+  .pipe(browserSync.stream());
 });
 
 
-gulp.task('watch', function(){
-  livereload.listen();
-  gulp.watch('./src/css/*.css', ['css']);
-  gulp.watch('./src/js/*.js', ['script']);
-  gulp.watch('./src/*.html', ['fileinclude']);
-  gulp.watch(['./dist/**/*.*']).on('change', livereload.changed);
-});
 
-gulp.task('default', ['css', 'script', 'image', 'font', 'fileinclude', 'watch']);
+gulp.task('default', ['browser-sync']);
